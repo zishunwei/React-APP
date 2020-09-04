ead772bad91c0690f65bd2346b67e713309f07c4
zishunwei <zishun_wei@outlook.com>
2020-09-04 04:11:06 +0100

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>My First React Component!</h1>
+        <ToDo />
+        <ShoppingCart />
+      </div>
+    );
+  }
+}
+
+const List = (props) => {
+  return <p>{props.tasks.join(", ")}</p>;
+};
+
+class ToDo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>To Do Lists</h1>
+        <h2>Today</h2>
+        {/* change code below this line */}
+        <List tasks={["walk dog", "workout"]} />
+        <h2>Tomorrow</h2>
+        <List tasks={["walk dog", "workout", "study"]} />
+        {/* change code above this line */}
+      </div>
+    );
+  }
+}
+
+const Items = (props) => {
+  return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>;
+};
+
+Items.defaultProps = {
+  quantity: 0,
+};
+
+class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    {
+      /* change code below this line */
+    }
+    return <Items quantity={10} />;
+    {
+      /* change code above this line */
+    }
+  }
 }
 
 export default App;
