643e550a3fde82e594a00279c7dda9fcf0414275
zishunwei <zishun_wei@outlook.com>
2020-09-04 04:52:57 +0100

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,66 +1,12 @@
 import React from "react";
 import "./App.css";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>My First React Component!</h1>
-        <ToDo />
-        <ShoppingCart />
-      </div>
-    );
-  }
-}
-
-const List = (props) => {
-  return <p>{props.tasks.join(", ")}</p>;
+const App = () => {
+  return (
+    <div className="App">
+      <h1>My First React Component!</h1>
+    </div>
+  );
 };
-
-class ToDo extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div>
-        <h1>To Do Lists</h1>
-        <h2>Today</h2>
-        {/* change code below this line */}
-        <List tasks={["walk dog", "workout"]} />
-        <h2>Tomorrow</h2>
-        <List tasks={["walk dog", "workout", "study"]} />
-        {/* change code above this line */}
-      </div>
-    );
-  }
-}
-
-const Items = (props) => {
-  return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>;
-};
-
-Items.defaultProps = {
-  quantity: 0,
-};
-
-class ShoppingCart extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    {
-      /* change code below this line */
-    }
-    return <Items quantity={10} />;
-    {
-      /* change code above this line */
-    }
-  }
-}
 
 export default App;
